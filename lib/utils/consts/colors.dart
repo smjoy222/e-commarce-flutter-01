8036170c618488864de98c343b2b26e3dec9b0f7
smjoy222 <smjoy222@gmail.com>
2025-03-11 10:11:21 +0600

    basic

---
+++ lib/utils/consts/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  static const primary = Color(0xFF5CC936);
+  static const primary1 = Color(0xFFFFC120);
+  static const primary2 = Color(0xFF0C1A30);
+  static const primary3 = Color(0xFFFFFFFF);
+}
